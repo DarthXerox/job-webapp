f5ceae185a0e8447d09f40bb8de2fea73a8fe09c
Jan Pokorny <JenomPokorny@gmail.com>
2020-10-27 20:44:55 +0100

    Implement Repository and UnitOfWork

---
+++ DAL/Repository.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL
+{
+    public class Repository<TEntity> where TEntity : class, new()
+    {
+        private readonly JobDbContext context;
+        private readonly DbSet<TEntity> dbSet;
+
+        public Repository(JobDbContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public virtual IQueryable<TEntity> GetAll()
+        {
+            return context.Set<TEntity>();
+        }
+
+        public virtual TEntity GetById(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public virtual void Add([NotNull] TEntity entity)
+        {
+            if (entity == null) throw new ArgumentNullException($"{nameof(Add)} entity must not be null");
+            dbSet.Add(entity);
+        }
+
+        public virtual async Task<TEntity> AddAsync([NotNull] TEntity entity)
+        {
+            if (entity == null) throw new ArgumentNullException($"{nameof(AddAsync)} entity must not be null");
+            await context.AddAsync(entity);
+            return entity;
+        }
+
+        public virtual void Update([NotNull] TEntity entity)
+        {
+            if (entity == null) throw new ArgumentNullException($"{nameof(Update)} entity must not be null");
+            dbSet.Attach(entity);
+            context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public virtual void Delete(int id)
+        {
+            Delete(dbSet.Find(id));
+        }
+
+        public virtual void Delete([NotNull] TEntity entity)
+        {
+            if (entity == null) throw new ArgumentNullException($"{nameof(Delete)} entity must not be null");
+            if (context.Entry(entity).State == EntityState.Detached) dbSet.Attach(entity);
+            dbSet.Remove(entity);
+        }
+    }
+}
---
+++ DAL/UnitOfWork.cs
@@ -0,0 +1,45 @@
+using System;
+using DAL.Entities;
+
+namespace DAL
+{
+    public sealed class UnitOfWork : IDisposable
+    {
+        private readonly JobDbContext context = new JobDbContext();
+
+        private Repository<Company> companyRepository;
+        private Repository<JobApplication> jobApplicationRepository;
+        private Repository<JobApplicationAnswer> jobApplicationAnswerRepository;
+        private Repository<JobOffer> jobOfferRepository;
+        private Repository<JobOfferQuestion> jobOfferQuestionRepository;
+        private Repository<JobOfferSkill> jobOfferSkillRepository;
+        private Repository<JobSeeker> jobSeekerRepository;
+        private Repository<JobSeekerSkill> jobSeekerSkillRepository;
+        private Repository<Skill> skillRepository;
+
+        public Repository<Company> CompanyRepository => companyRepository ??= new Repository<Company>(context);
+        public Repository<JobApplication> JobApplicationRepository => jobApplicationRepository ??= new Repository<JobApplication>(context);
+        public Repository<JobApplicationAnswer> JobApplicationAnswerRepository => jobApplicationAnswerRepository ??= new Repository<JobApplicationAnswer>(context);
+        public Repository<JobOffer> JobOfferRepository => jobOfferRepository ??= new Repository<JobOffer>(context);
+        public Repository<JobOfferQuestion> JobOfferQuestionRepository => jobOfferQuestionRepository ??= new Repository<JobOfferQuestion>(context);
+        public Repository<JobOfferSkill> JobOfferSkillRepository => jobOfferSkillRepository ??= new Repository<JobOfferSkill>(context);
+        public Repository<JobSeeker> JobSeekerRepository => jobSeekerRepository ??= new Repository<JobSeeker>(context);
+        public Repository<JobSeekerSkill> JobSeekerSkillRepository => jobSeekerSkillRepository ??= new Repository<JobSeekerSkill>(context);
+        public Repository<Skill> SkillRepository => skillRepository ??= new Repository<Skill>(context);
+
+        public void SaveChanges()
+        {
+            context.SaveChanges();
+        }
+
+        public async void SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
