8bef985b11a16ea9b9991cfebe4d8bd1b095cd5e
Martin Bučo <485628@muni.cz>
2021-01-09 17:20:29 +0100

    Added QueryObjects tests

--- UnitTests/Business/BusinessTests.cs
+++ UnitTests/Business/BusinessTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
 using System.Linq;
 using System.Text;
 using Business.QueryObjects;
 using DAL;
 using DAL.Entities;
+using DAL.Enums;
 using Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
@@ -25,11 +27,17 @@ namespace UnitTests.Business
             .UseInMemoryDatabase(databaseName: "Test")
             .Options;
 
+        private static int counter = 0;
+        private DbContextOptions<JobDbContext> GetInMemoryOptions() =>
+            new DbContextOptionsBuilder<JobDbContext>()
+                .UseInMemoryDatabase(databaseName: $"Test{++counter}")
+                .Options;
+
         [Fact]
-        public void QueryObjectsCorrect()
+        public void CompanyQueryObjectTest()
         {
             // Companies
-            var unitOfWork = new UnitOfWork(inMemoryOptions);
+            var unitOfWork = new UnitOfWork(GetInMemoryOptions());
             Seeder.Seed(unitOfWork);
             var x = unitOfWork.CompanyRepository.GetAll();
             Assert.Equal(4, unitOfWork.CompanyRepository.GetAll().Count());
@@ -59,12 +67,12 @@ namespace UnitTests.Business
         [Fact]
         public void JobApplicationQueryObjectTest()
         {
-            var unit = new UnitOfWork(inMemoryOptions);
+            var unit = new UnitOfWork(GetInMemoryOptions());
             Seeder.Seed(unit);
 
-            unit.JobOfferQuery.FilterByCompanyName("Apple"); // Apple should have 2 applications
+            unit.JobOfferQuery.FilterByCompanyName("Apple").OrderBy(o => o.Name, false);
             var offers = unit.JobOfferQuery.ExecuteAsync().Result;
-            Assert.Equal(2, offers.Count());
+            Assert.Equal(2, offers.Count()); // Apple should have 1 offer
             var offer = offers.First();
             Assert.NotNull(offer);
             Assert.Equal("Well-paid position at Apple", offer.Name);
@@ -73,12 +81,74 @@ namespace UnitTests.Business
             Assert.Equal(2, queryObject.GetByJobOfferIdAsync(offer.Id ?? -1)
                     .Result
                     .Count());
-            Assert.Equal(new List<string>() {"Jason", "Neo"},
+            Assert.Equal(new List<string>() { "Jason", "Neo" },
                 queryObject.GetByJobOfferIdAsync(offer.Id ?? -1)
                     .Result
                     .Select(a => a.Applicant?.Name));
-            unit.Dispose();
+            Assert.Equal(new List<string>() { "Neo" },
+                queryObject.GetByJobOfferIdAndStatusAsync(offer.Id ?? -1, Status.Rejected)
+                    .Result
+                    .Select(a => a.Applicant?.Name));
 
+            int neoId = queryObject.GetByJobOfferIdAndStatusAsync(offer.Id ?? -1, Status.Rejected)
+                .Result
+                .Select(a => a.Applicant?.Id)
+                .First() ?? -1;
+
+            Assert.Equal(new List<string>() {"Microsoft > Apple", "Blue pill, red pill, I'll eat both"},
+                queryObject.GetByApplicantIdAsync(neoId)
+                    .Result
+                    .Select(a => a.Text));
+
+            Assert.Equal(new List<string>() {"Blue pill, red pill, I'll eat both"},
+                queryObject.GetByApplicantIdAndStatusAsync(neoId, Status.Rejected)
+                                .Result
+                                .Select(a => a.Text));
+
+            unit.Dispose();
+        }
+
+        [Fact]
+        public void JobOfferQueryObjectTest()
+        {
+            var unit = new UnitOfWork(GetInMemoryOptions());
+            Seeder.Seed(unit);
+
+            unit.CompanyQuery.FilterByName("Apple");
+            var apple = unit.CompanyQuery.ExecuteAsync().Result.First();
+
+            var queryObject = new JobOfferQueryObject(unit);
+            Assert.Equal(new List<string>()
+                {
+                    "Cybertruck driving software development at Tesla",
+                    "Software development at Apple"
+                },
+                queryObject.GetByNameContainsAsync("oftware")
+                    .Result
+                    .Select(o => o.Name));
+
+            Assert.Equal(2, queryObject.GetByCompanyNameAsync("Apple")
+                .Result
+                .Count());
+
+            Assert.Equal(new List<string>()
+                {
+                    "Developing new .NET Core SDK",
+                    "Well-paid position at Apple"
+                },
+                queryObject.GetBySkillTagAsync("C#")
+                        .Result
+                        .Select(o => o.Name));
+
+            Assert.Equal(new List<string>()
+                {
+                    "Developing new .NET Core SDK",
+                    "Pouring coffee and writing names on cups"
+                },
+                queryObject.GetByCityAsync("Seattle")
+                    .Result
+                    .Select(o => o.Name));
+            unit.Dispose();
         }
     }
 }
